3f891aaeda1a97569166c01e6a5460447551b754
LimaTarcisio <tarcisio.pesquisa.estudo@gmail.com>
2025-03-06 12:43:27 -0300

    testes finalizados

--- banco de dados/testes.py
+++ banco de dados/testes.py
@@ -6,6 +6,7 @@ cursor.close()
 conexao.close()
 '''
 
+tabelas_criadas = 1
 
 import mysql.connector
 
@@ -24,46 +25,49 @@ cursor.close()
 conexao.close()
 
 conexao = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "54321",
-    database = "db_testes"
-)
+        host = "localhost",
+        user = "root",
+        password = "54321",
+        database = "db_testes"
+    )
 
 cursor = conexao.cursor()
 
-tabelas = ["CREATE TABLE tb_funcionario ", 
-           "CREATE TABLE tb_cliente "]
+if tabelas_criadas == 0:
+    
 
-atributo_func = [   "(",
-                    "id_func INT AUTO_INCREMENT PRIMARY KEY,",
-                    "cpf INT(11) UNIQUE NOT NULL,",
-                    "nome VARCHAR(50) NOT NULL,",
-                    "hora_entrada TIME,",
-                    "hora_saida TIME",
-                    ");"
-                ]
+    tabelas = ["CREATE TABLE tb_funcionario ", 
+            "CREATE TABLE tb_cliente "]
 
-atributo_clie = [   "(",
-                    "id_cliente INT AUTO_INCREMENT PRIMARY KEY,",
-                    "nome VARCHAR(50) NOT NULL,",
-                    "endereco VARCHAR(100)",
-                    ");"
-                ]
+    atributo_func = [   "(",
+                        "id_func INT AUTO_INCREMENT PRIMARY KEY,",
+                        "cpf INT(11) UNIQUE NOT NULL,",
+                        "nome VARCHAR(50) NOT NULL,",
+                        "hora_entrada TIME,",
+                        "hora_saida TIME",
+                        ");"
+                    ]
 
-tabelas[0] += " ".join(atributo_func)
-tabelas[1] += " ".join(atributo_clie)
+    atributo_clie = [   "(",
+                        "id_cliente INT AUTO_INCREMENT PRIMARY KEY,",
+                        "nome VARCHAR(50) NOT NULL,",
+                        "endereco VARCHAR(100)",
+                        ");"
+                    ]
+
+    tabelas[0] += " ".join(atributo_func)
+    tabelas[1] += " ".join(atributo_clie)
 
 
 
-for tabela in tabelas:
-    cursor.execute(tabela)
+    for tabela in tabelas:
+        cursor.execute(tabela)
 
-conexao.commit()
-cursor.close()
-conexao.close()
+    conexao.commit()
+    cursor.close()
+    conexao.close()
 
-'''
+    '''
 para "simplificar", (porque não está nada simplificado kkkk)
 poderia também ser feito em uma matriz os atributos, dessa forma,
 em um ambiente com muitas tabelas poderiam ser enseridos seus atributos a seus respectivos posição no for,
@@ -98,3 +102,32 @@ conexao.commit()
 cursor.close()
 conexao.close()
 '''
+
+cursor.execute("TRUNCATE TABLE tb_cliente;")
+
+insert_cli = "INSERT INTO tb_cliente (nome, endereco) VALUES "
+
+dados = [
+            "('Cleber James', 'Rua B98');",
+            "('Antonio','central');",
+            "('Ana Maria','num sei');",
+            "('Weverton','gravile');",
+            "('Javsiu','heavem');",
+            "('Ednalva','sol');"
+        ]
+
+for dado in dados:
+    querry = insert_cli+dado
+    cursor.execute(querry) 
+
+conexao.commit()
+
+cursor.execute("SELECT * FROM tb_cliente")
+clientes = cursor.fetchall()
+
+for cliente in clientes:
+    print(cliente)
+
+
+cursor.close()
+conexao.close()
